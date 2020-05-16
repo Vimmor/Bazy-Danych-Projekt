20ee63a1e3a202a5bd31e16578f3f14c91c0c49d
Vimmor <luczmateusz@gmail.com>
2020-05-17 01:11:55 +0200

    Create simple trigger and users

--- bazy_danych.sql
+++ bazy_danych.sql
@@ -701,4 +701,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-16 11:10:28
+-- Dump completed on 2020-05-17  1:10:09
