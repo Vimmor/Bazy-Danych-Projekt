c8893290f43704759b8bd599db49968947d96331
Student 241540 <241540@student.pwr.edu.pl>
2020-06-01 13:37:47 +0200

    Add main window layouts

--- InternetShopProject/ClientMenu.xaml.cs
+++ InternetShopProject/ClientMenu.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,18 +14,13 @@ using System.Windows.Shapes;
 namespace InternetShopProject
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ClientMenu.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ClientMenu : Page
     {
-        bool logIn = false;
-        char user; // 'w' - worker, 'c' - client, 'a' - admin
-
-        public MainWindow()
+        public ClientMenu()
         {
             InitializeComponent();
-
-            Bar.Content = new LogOutBar();
         }
     }
 }
--- InternetShopProject/LogOutBar.xaml.cs
+++ InternetShopProject/LogOutBar.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,18 +14,13 @@ using System.Windows.Shapes;
 namespace InternetShopProject
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LogOutBar.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LogOutBar : Page
     {
-        bool logIn = false;
-        char user; // 'w' - worker, 'c' - client, 'a' - admin
-
-        public MainWindow()
+        public LogOutBar()
         {
             InitializeComponent();
-
-            Bar.Content = new LogOutBar();
         }
     }
 }
