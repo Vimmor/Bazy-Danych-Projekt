d19b23c7e242e068efa8290a76c8275bc23ab325
Vimmor <luczmateusz@gmail.com>
2020-06-07 21:59:36 +0200

    Method to get invoices

---
+++ InternetShopProject/Database/Logic/Invoice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InternetShopProject.Database.Logic
+{
+    public class Invoice
+    {
+        private static shop_databaseContext context = new shop_databaseContext();
+
+        public static List<Faktury> getAllInvoices() {
+            return context.Faktury.ToList();
+        }
+
+        public static List<Fakturydlapracownika> getInvoicesForEmployer() {
+            return context.Fakturydlapracownika.ToList();
+        }
+    }
+}
