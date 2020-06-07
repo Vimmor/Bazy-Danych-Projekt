16fa409e22096417fe8c68ce8d81b67e04f1bc42
Vimmor <luczmateusz@gmail.com>
2020-06-07 21:24:55 +0200

    Method to get orders for client specified by ID

--- InternetShopProject/Database/Logic/OrdersForClient.cs
+++ InternetShopProject/Database/Logic/OrdersForClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace InternetShopProject.Database.Logic
@@ -8,6 +9,18 @@ namespace InternetShopProject.Database.Logic
     {
         private static shop_databaseContext context = new shop_databaseContext();
 
-
+        public static List<Zamowieniadlaklienta> getOrdersForClientById(int id) {
+            var ordersList = context.Zamowienia.Where(o => o.IdKlienta == id).ToList();
+            var ordersForClient = new List<Zamowieniadlaklienta>();
+            foreach (var order in ordersList) {
+                ordersForClient.Add(new Zamowieniadlaklienta()
+                {
+                    IdZamowienia = order.IdZamowienia,
+                    DataTransakcji = order.DataTransakcji,
+                    Status = OrdersForEmployer.getOrderStatusById(order.IdStatusZamowienia)
+                });
+            }
+            return ordersForClient;
+        }
     }
 }
